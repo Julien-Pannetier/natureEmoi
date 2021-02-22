645dcbd48fcb7a491e9d10c216654305dff127e8
Julien Pannetier <pannetier.j@hotmail.fr>
2021-02-22 11:16:10 +0100

    Intégration du menu du site BlackMadre.com

--- src/index.js
+++ src/index.js
@@ -1,2 +1,40 @@
 import "./styles.scss";
-console.log("hello world!");
+
+const openMenu = document.querySelector('#nav-top-right-open');
+const closeMenu = document.querySelector('#nav-top-right-close');
+
+openMenu.addEventListener('click', () => { 
+    document.body.classList.add('menu-open');
+})
+
+closeMenu.addEventListener('click', () => {
+    document.body.classList.remove('menu-open');
+})
+
+
+const links = document.querySelectorAll('#menu-inner-right-container a');
+const images = document.querySelectorAll('.menu-inner-left-img');
+
+let currentZIndex = 3;
+let currentImage = images[0];
+
+links.forEach(link => {
+    link.addEventListener('mouseenter', () => {
+        const image = [...images].find(image => {
+            return image.dataset.id === link.dataset.id
+        })
+
+        if(currentImage === link) return
+
+        image.style.transition = "null";
+        image.style.opacity = 0;
+        image.style.zIndex = currentZIndex++;
+
+        setTimeout(() => {
+            image.style.opacity = 1;
+            image.style.transition = "opacity 700ms ease";
+        }, 1);
+
+        currentImage = link;
+    })
+})
